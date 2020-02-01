a59a9ca8c4174429f3f95da34a6c67894d4f4bd3
valthun <sp17f1re@byui.edu>
2020-02-01 07:29:01 -0500

    commit

--- lesson4/js/current date.js
+++ lesson4/js/current date.js
@@ -1,21 +1,35 @@
 var year = new Date().getFullYear();
+
 var m = new Date();
 var month = new Array();
-month[0] = "01";
-month[1] = "02";
-month[2] = "03";
-month[3] = "04";
-month[4] = "05";
-month[5] = "06";
-month[6] = "07";
-month[7] = "08";
-month[8] = "09";
-month[9] = "10";
-month[10] = "11";
-month[11] = "12";
+month[0] = "January";
+month[1] = "February";
+month[2] = "March";
+month[3] = "April";
+month[4] = "May";
+month[5] = "June";
+month[6] = "July";
+month[7] = "August";
+month[8] = "September";
+month[9] = "October";
+month[10] = "November";
+month[11] = "December";
 var mo = month[m.getMonth()];
+
 var day = new Date().getDate();
 
-document.getElementById("currentyear").innerHTML=year;
-document.getElementById("currentmonth").innerHTML=mo;
+var w = new Date();
+var week = new Array(7);
+week[0] = "Sunday";
+week[1] = "Monday";
+week[2] = "Tuesday";
+week[3] = "Wednsday";
+week[4] = "Thursday";
+week[5] = "Friday";
+week[6] = "Saturday";
+var we = week[w.getDay()];
+
+document.getElementById("weekday").innerHTML=we;
 document.getElementById("currentday").innerHTML=day;
+document.getElementById("currentmonth").innerHTML=mo;
+document.getElementById("currentyear").innerHTML=year;
