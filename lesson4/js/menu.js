7ece9c55fafc45f8c99a59bae097b97900e6a195
valthun <sp17f1re@byui.edu>
2020-01-31 22:36:43 -0500

    commit

---
+++ lesson4/js/menu.js
@@ -0,0 +1,8 @@
+function myFUnction() {
+    var n = document.getElementById("hidden");
+    if (n.style.display === "block") {
+        n.style.display = "none";
+    } else {
+        n.style.display = "block";
+    }
+}
