90f9aa768fe52c823842c8e6cfcab7bfd1c4cfb2
valthun <sp17f1re@byui.edu>
2020-02-15 12:21:05 -0500

    commit

--- lesson6/js/winchill.js
+++
@@ -1,5 +0,0 @@
-
-    var t = document.getElementById("temp");
-    var s = document.getElementById("speed");
-    var c = 35.74 + 0.6215*t + (0.4275*t -35.75) * Math.pow(s, 0.16);
-    document.getElementById("chill").innerHTML=c;
