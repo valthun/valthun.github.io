6bdf1ab2532502bd57bc97894aec2a976556a899
valthun <sp17f1re@byui.edu>
2020-03-04 14:47:01 -0500

    commit

--- lesson9/js/towninfo.js
+++ lesson9/js/towninfo.js
@@ -13,13 +13,20 @@ fetch(requestURL)
       
       let towns = document.createElement('section');
       let h2 = document.createElement('h2');
+      let founded = document.createElement('p');
       let motto = document.createElement('p');
-      let 
+      let rainfall = document.createElement('p');
 
       h2.textContent = towninfo[i].town;
+      founded.textContent = towninfo[i].founded;
+      motto.textContent = towninfo[i].motto;
+
+      rainfall.textContent = towninfo[i].rainfall;
 
 
       towns.appendChild(h2);
+      towns.appendChild(founded);
+      towns.appendChild(motto);
 
       document.querySelector('div.towns').appendChild(towns);
     }
