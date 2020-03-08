60da3d485d4231308786f190225f8949b49e8882
valthun <sp17f1re@byui.edu>
2020-03-07 22:21:07 -0500

    commit

--- lesson9/js/prophets.js
+++ lesson9/js/prophets.js
@@ -5,18 +5,18 @@ fetch(requestURL)
     return response.json();
   })
   .then(function (jsonObject) {
-    console.table(jsonObject);  // temporary checking for valid response and data parsing
-    
+    console.table(jsonObject); // temporary checking for valid response and data parsing
+
     const prophets = jsonObject['prophets'];
 
-    for (let i = 0; i < prophets.length; i++ ) {
-      
+    for (let i = 0; i < prophets.length; i++) {
+
       let card = document.createElement('section');
       let h2 = document.createElement('h2');
       let birth = document.createElement('p');
       let place = document.createElement('p');
       let image = document.createElement('img');
-    
+
 
       h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
       birth.textContent = "Date of Birth: " + prophets[i].birthdate;
@@ -34,4 +34,3 @@ fetch(requestURL)
       document.querySelector('div.cards').appendChild(card);
     }
   });
-
