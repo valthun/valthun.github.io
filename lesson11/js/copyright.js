1894bb04b2e611a5b07cc917042b6f2875f4229b
valthun <sp17f1re@byui.edu>
2020-03-14 22:03:58 -0400

    commit

---
+++ lesson11/js/copyright.js
@@ -0,0 +1,4 @@
+//put current year with copyright//
+
+ var year=new Date().getFullYear();
+ document.getElementById("copyyear").innerHTML=year;
---
+++ lesson11/js/popup.js
@@ -0,0 +1,5 @@
+var b = new Date();
+pancake = b.getDay();
+if (pancake === 5) {
+    document.getElementById('pancake').style.display = "block";
+}
