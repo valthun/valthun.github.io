17005fab561effc0f4bacc9dee60108e0e1630ec
valthun <sp17f1re@byui.edu>
2020-03-19 22:33:54 -0400

    commit

--- lesson11/js/windchill_sodasprings.js
+++ lesson11/js/windchill_sodasprings.js
@@ -48,12 +48,9 @@ fetch(apiURL)
 
         for (let i = 0; i < fiveday.length; i++) {
 
-            document.getElementById('fore1').textContent = fiveday[0].main.temp;
-            document.getElementById('fore2').textContent = fiveday[1].main.temp;
-            document.getElementById('fore3').textContent = fiveday[2].main.temp;
-            document.getElementById('fore4').textContent = fiveday[3].main.temp;
-            document.getElementById('fore5').textContent = fiveday[4].main.temp;
-
+            let x = i + 1;
+        
+            document.getElementById('fore'+x).textContent = fiveday[i].main.temp;
 
             var day = new Date(fiveday[i].dt_txt);
             var week = new Array(7);
@@ -66,37 +63,12 @@ fetch(apiURL)
             week[6] = "Sat";
             var we = week[day.getDay()];
 
+            document.getElementById('weekday'+x).innerHTML = we;
 
-            document.getElementById('weekday1').innerHTML = we;
-            document.getElementById('weekday2').innerHTML = we;
-            document.getElementById('weekday3').innerHTML = we;
-            document.getElementById('weekday4').innerHTML = we;
-            document.getElementById('weekday5').innerHTML = we;
-
-            const imagesrc1 = 'https://openweathermap.org/img/w/' + fiveday[0].weather[0].icon + '.png';
+            const imagesrc1 = 'https://openweathermap.org/img/w/' + fiveday[i].weather[0].icon + '.png';
             const desc1 = jsObject.list[0].weather[0].description;
-            document.getElementById('icon1').setAttribute('src', imagesrc1);
-            document.getElementById('icon1').setAttribute('alt', desc1);
-
-            const imagesrc2 = 'https://openweathermap.org/img/w/' + fiveday[1].weather[0].icon + '.png';
-            const desc2 = jsObject.list[1].weather[0].description;
-            document.getElementById('icon2').setAttribute('src', imagesrc2);
-            document.getElementById('icon2').setAttribute('alt', desc2);
-
-            const imagesrc3 = 'https://openweathermap.org/img/w/' + fiveday[2].weather[0].icon + '.png';
-            const desc3 = jsObject.list[2].weather[0].description;
-            document.getElementById('icon3').setAttribute('src', imagesrc3);
-            document.getElementById('icon3').setAttribute('alt', desc3);
-
-            const imagesrc4 = 'https://openweathermap.org/img/w/' + fiveday[1].weather[0].icon + '.png';
-            const desc4 = jsObject.list[3].weather[0].description;
-            document.getElementById('icon4').setAttribute('src', imagesrc4);
-            document.getElementById('icon4').setAttribute('alt', desc4);
-
-            const imagesrc5 = 'https://openweathermap.org/img/w/' + fiveday[1].weather[0].icon + '.png';
-            const desc5 = jsObject.list[4].weather[0].description;
-            document.getElementById('icon5').setAttribute('src', imagesrc5);
-            document.getElementById('icon5').setAttribute('alt', desc5);
+            document.getElementById('icon'+x).setAttribute('src', imagesrc1);
+            document.getElementById('icon'+x).setAttribute('alt', desc1); 
 
         }
 
