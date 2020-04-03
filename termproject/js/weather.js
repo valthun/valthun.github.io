6d1c84b0e281353986e4ee76f6e87a518264dc39
valthun <sp17f1re@byui.edu>
2020-04-02 22:13:48 -0400

    commit

---
+++ termproject/js/weather.js
@@ -0,0 +1,11 @@
+const apiURL = 'https://api.openweathermap.org/data/2.5/weather?id=4092267&units=imperial&APPID=81785cac2327d15c890f8bdde7a8bf16';
+
+fetch(apiURL)
+.then((response) => response.json())
+    .then((jsObject) => {
+        console.log(jsObject);
+
+        document.getElementById('temp').textContent = jsObject.main.feels_like;
+        document.getElementById('looks').textContent = jsObject.weather[0].description;
+        document.getElementById('speed').textContent = jsObject.wind.speed;
+    });
