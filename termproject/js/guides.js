edeacad389f38f3371ab58cc0aaf1cc6d178521a
valthun <sp17f1re@byui.edu>
2020-04-02 23:57:14 -0400

    commit

--- termproject/js/guides.js
+++ termproject/js/guides.js
@@ -12,12 +12,15 @@ fetch(guideURL)
             let card = document.createElement('section')
             let name = document.createElement('h3');
             let bio = document.createElement('p');
+            let cert = document.createElement('p');
 
             name.textContent = "Name: " + guides[i].firstName + " " + guides[i].nickName + " " + guides[i].lastName;
             bio.textContent = "Bio: " + guides[i].bio;
+            cert.textContent = "Certifications: " + guides[i].certs;
 
             card.appendChild(name);
             card.appendChild(bio);
+            card.appendChild(cert);
 
             document.querySelector('div.guides').appendChild(card);
         }
